455c1334ec88e2d910a49f134e13a2b6277e4bdb
Jessenia Toapanta <jessytpt.dev@gmail.com>
2022-05-06 18:24:54 -0500

    add link on heroku

--- src/main/java/com/webfluxdemo/WebfluxDemoApplication.java
+++ src/main/java/com/webfluxdemo/WebfluxDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebfluxDemoApplication {
 
 	public static void main(String[] args) {
-
+		// Link de heroku: https://api-webflux-start.herokuapp.com/customers/all
 		SpringApplication.run(WebfluxDemoApplication.class, args);
 	}
 
