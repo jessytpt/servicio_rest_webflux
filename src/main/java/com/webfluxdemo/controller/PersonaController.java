37bd0d5ab3a4fd7034a518ba453c6674d0a30942
Jessenia Toapanta <jessytpt.dev@gmail.com>
2022-05-06 17:57:32 -0500

    fix endpoints

--- src/main/java/com/webfluxdemo/controller/PersonaController.java
+++ src/main/java/com/webfluxdemo/controller/PersonaController.java
@@ -5,10 +5,9 @@ import com.webfluxdemo.model.Persona;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /*
 Hay dos posibilidades de hacer un servicio con reactividad
@@ -18,19 +17,46 @@ Hay dos posibilidades de hacer un servicio con reactividad
  */
 
 @RestController
-@RequestMapping("/personas")
+@RequestMapping("/customers")
 public class PersonaController {
     private static Logger log = LoggerFactory.getLogger(PersonaController.class);
 
     @Autowired
     private IPersonaRepository personaRepository;
 
-    @GetMapping
+    @GetMapping("/all")
     public Flux<Persona> listar()
     {
        return personaRepository.listar();
 
     }
+
+    @PostMapping("/new")
+    public Mono<Persona> registrar(@RequestBody Persona persona){
+        return personaRepository.registrar(persona);
+    }
+
+    @PutMapping("/save")
+    public Mono<Persona> modificar(@RequestBody Persona persona){
+        return personaRepository.modificar(persona);
+    }
+
+    @GetMapping("/product/{idProduct}")
+    public Mono<Persona> listarPorId(@PathVariable("idProduct") int idProduct){
+        return personaRepository.listarPorId(idProduct);
+    }
+
+    @DeleteMapping("/delete/{Id}")
+    public Mono<Void> eliminar(@PathVariable("Id") int Id) {
+        // Comprueba sì primero existe el elemento y luego lo elimina
+        return personaRepository.listarPorId(Id)
+                .flatMap( p -> personaRepository.eliminar(p.getIdPersona()));
+    }
+
+
+
+
+
 
 
 }
